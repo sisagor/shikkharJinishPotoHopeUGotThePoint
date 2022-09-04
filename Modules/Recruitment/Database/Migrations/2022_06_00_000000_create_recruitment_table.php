4b3850164ba4d6faf9a9fa4caeb777ef3bec2751
sisagor <sisagor767@gmail.com>
2022-09-04 18:39:43 +0600

    edit cms migrration

--- Modules/Recruitment/Database/Migrations/2022_06_00_000000_create_recruitment_table.php
+++ Modules/Recruitment/Database/Migrations/2022_06_00_000000_create_recruitment_table.php
@@ -103,8 +103,9 @@ class CreateRecruitmentTable extends Migration
         if (! Schema::hasTable('cms')) {
             Schema::create('cms', function (Blueprint $table) {
                 $table->id();
-                $table->string('key')->nullable();
+                $table->string('type')->nullable();
                 $table->json('content')->nullable();
+                $table->tinyInteger('status')->default(0)->nullable();
                 $table->timestamps();
             });
         }
