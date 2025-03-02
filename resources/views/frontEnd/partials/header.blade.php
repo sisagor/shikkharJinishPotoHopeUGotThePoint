39522d75fb580c219c7c26a110076555b407527b
Md Rejwanul Hoque <bulbul.rej@gmail.com>
2025-03-02 23:03:03 +0600

    update design home page

--- resources/views/frontEnd/partials/header.blade.php
+++ resources/views/frontEnd/partials/header.blade.php
@@ -1,8 +1,16 @@
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-        <img src="{{asset('./frontEnd/img/Logo.png')}}" width="30" height="30" alt="">
+        <img src="{{asset('./frontEnd/img/Logo.png')}}" width="30" height="30" alt="Logo">
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+    >
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
@@ -14,7 +22,7 @@
                 <a class="nav-link" href="#">Kids</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Audits</a>
+                <a class="nav-link" href="#">Adults</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Generator</a>
@@ -22,16 +30,22 @@
             <li class="nav-item">
                 <a class="nav-link" href="#">Seasonal</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('blog.cat')}}">Blogs</a>
-            </li>
         </ul>
-        <div class="nav_right">
+        <div class="nav_right ml-auto">
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn  my-2 my-sm-0" type="submit"><img src="{{asset('./frontEnd/img/search-01.png')}}" alt="search"/></button>
+                <input
+                        class="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                >
+                <button class="btn s_btn my-2 my-sm-0" type="submit">
+                    <img src="{{asset('./frontEnd/img/search-01.png')}}" alt="search" />
+                </button>
             </form>
-            <a href="#" class="sign-in-button">Sign in</a>
+            <a href="#" class="sign-in-button ml-2">Sign in</a>
         </div>
     </div>
 </nav>
+
+
--- resources/views/frontEnd/sections/section_two.blade.php
+++ resources/views/frontEnd/sections/section_two.blade.php
@@ -7,14 +7,18 @@
     <div class="col-md-6 col-sm-6">
         <div class="our-author" style="background-color: #EFEBFF;">ABOUT US</div>
         <h2>Your Trusted Partner in Engaging Education</h2>
-        <p>We believe that every child deserves access to fun, engaging, and effective learning resources. That's why we created Print & USe, a treasure trove of downloadable activities, printable books, and interactive tools</p>
+        <p>We believe that every child deserves access to fun, engaging, and effective learning resources.
+            That's why we created Print & USe, a treasure trove of downloadable activities, printable books,
+            and interactive tools</p>
         <div class="about_widget">
             <div class="about_widget_left">
                 <img src="{{asset('/frontEnd/img/fi_92320841.png')}}" alt="" />
             </div>
             <div class="about_widget_right">
                 <h4>Created by Educators</h4>
-                <p>Our resources are developed by experienced teachers and early childhood specialists, ensuring they're aligned with educational standards and promote essential skills.</p>
+                <p>Our resources are developed by experienced teachers and early childhood
+                    specialists, ensuring they're aligned with educational standards and promote essential
+                    skills.</p>
 
             </div>
         </div>
@@ -24,7 +28,9 @@
             </div>
             <div class="about_widget_right">
                 <h4>Variety for Every Learner</h4>
-                <p>Discover a diverse collection of resources catering to different learning styles, subjects, and age groups. From colorful coloring pages to challenging puzzles and brain-teasing word games, we have something for everyone.</p>
+                <p>Discover a diverse collection of resources catering to different learning
+                    styles, subjects, and age groups. From colorful coloring pages to challenging puzzles
+                    and brain-teasing word games, we have something for everyone.</p>
 
             </div>
         </div>
@@ -34,12 +40,15 @@
             </div>
             <div class="about_widget_right">
                 <h4>Free and Accessible</h4>
-                <p>We believe all children deserve a chance to learn, regardless of background. That's why we offer our resources completely free of charge, making them accessible to everyone.</p>
+                <p>We believe all children deserve a chance to learn, regardless of background. That's why
+                    we offer our resources completely free of charge, making them accessible to everyone.
+                </p>
 
             </div>
         </div>
         <button class="learn-more-button">
-            Learn More <img  src="{{asset('/frontEnd/img/ArrowUp.png')}}" alt="arro up" />
+            Learn More
+            <img src="{{asset('/frontEnd/img/ArrowUp.png')}}" alt="arrow up" />
         </button>
     </div>
 </div>
