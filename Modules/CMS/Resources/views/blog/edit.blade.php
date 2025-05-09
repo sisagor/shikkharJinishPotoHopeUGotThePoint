933a7b3e5c0c768aca9a1449681f40329e08a328
sisagor <sisagor767@gmail.com>
2025-05-10 00:21:34 +0600

    fixe shit again blog deails Cms

--- Modules/CMS/Resources/views/blog/edit.blade.php
+++ Modules/CMS/Resources/views/blog/edit.blade.php
@@ -260,60 +260,6 @@
 
       </div>
     {{--  @else--}}
-        <div id="dynamic-fields">
-            <div class="dynamic-block mt-3">
-                <div class="dynamic-block-header">
-                    Blog Details 
-                    <div class="text-end">
-                        <button type="button" class="btn btn-danger btn-sm float-right remove-field">Remove</button>
-                    </div>
-                </div>
-            <div class="row">
-                <div class="col-md-6 col-sm-6">
-                    <label class="col-form-label label-align" for="image">
-                        {{ trans('app.image') }} (Size should be 770x500)
-                        <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="left"
-                        title="{{ trans('help.image') }}"></i>
-                    </label>
-                    <div class="item form-group">
-                        <input type="file" class="form-control" name="images[]"  placeholder="{{ trans('app.image') }}">
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3">
-                    <label class="col-form-label label-align" for="image_alter">
-                        {{ trans('app.image_alter') }}
-                        <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="left"
-                        title="{{ trans('help.image_alter') }}"></i>
-                    </label>
-                    <div class="item form-group">
-                        <input type="text" class="form-control" name="images_alter[]"  placeholder="{{ trans('app.image_alter') }}">
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3">
-                    <label class="col-form-label label-align" for="details">
-                        {{ trans('app.order') }}
-                        <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="left" title="{{ trans('help.order') }}"></i>
-                    </label>
-                    <div class="item form-group">
-                        <select class="form-control" name="orders[]">
-                            <option value="">{{ trans('app.select') }}</option>
-                            @for($i = 1; $i <= 15; $i++)
-                                <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
-                        </select>
-                    </div>
-                </div>
-                <div class="col-md-12 col-sm-12">
-                    <label class="col-form-label label-align" for="details">
-                        {{ trans('app.details') }}
-                        <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="left" title="{{ trans('help.details') }}"></i>
-                    </label>
-                    <div class="item form-group">
-                        <textarea class="form-control editor" id="initialEditor" name="details[]" placeholder="{{ trans('app.details') }}"></textarea>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
    {{-- @endif--}}
@@ -322,73 +268,11 @@
 
 @endsection
 
+
 @section('formScripts')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
-    <script>
-        function initializeCKEditor(selector) {
-            // Ensure CKEditor is not already initialized on this textarea
-            if (!CKEDITOR.instances[selector.id]) {
-                CKEDITOR.replace(selector);
-            }
-        }
-
-        // Initialize CKEditor on all existing textareas when the document is ready
-        $(document).ready(function() {
-            // Initialize CKEditor for all existing textareas
-            $('.editor').each(function() {
-                initializeCKEditor(this);
-            });
-
-
-
-            $('#add-field').click(function()
-            {
-                // var newField = $('#dynamic-fields .dynamic-block:first').clone();
-                // newField.find('input, select, textarea').val('');
-                // newField.find('.remove-field').show();
-                // newField.find('#add-field').hide();
-                // $('#dynamic-fields').append(newField);
-                // CKEDITOR.replace(newTextarea[0]);
-
-                var newField = $('#dynamic-fields .dynamic-block:first').clone();
-
-                // Reset the values of input, select, and textarea
-                newField.find('input, select, textarea').val('');
-
-                // Show the remove button and hide the add button
-                newField.find('.remove-field').show();
-                newField.find('#add-field').hide();
-
-                // Append the new field to the container
-                $('#dynamic-fields').append(newField);
-
-                var newTextarea = newField.find('textarea')[0];
-
-                // Initialize CKEditor only if it's not already initialized
-                if (!CKEDITOR.instances[newTextarea.id]) {
-                    initializeCKEditor(newTextarea);
-                }
-            });
-
-
-            $(document).on('click', '.remove-field', function() {
-                $(this).closest('.dynamic-block').remove();
-            });
-
-            $(document).on('click', '.remove-field-exist', function() {
-                $(this).closest('.dynamic-block-exist').remove();
-            });
-
-            // Hide remove button for the first set of fields
-            //$('#dynamic-fields .remove-field').hide();
-
-
-        });
-    </script>
-
-
+    @include('cms::scripts.formScript')
 @endsection
+
 
 <style>
     .dynamic-block {
